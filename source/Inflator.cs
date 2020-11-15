c2b2351b71338ca4472dea1a10832d271d5204a4
Giles Papworth <giles@superchiefsoftware.com>
2020-11-15 17:59:30 +0000

    working on unit tests

---
+++ source/Inflator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Kyameru.Core.Contracts;
+
+namespace Kyameru.Component.File
+{
+    public class Inflator : IOasis
+    {
+        public IFromComponent CreateFromComponent(Dictionary<string, string> headers)
+        {
+            return new FileWatcher(headers);
+        }
+
+        public IToComponent CreateToComponent(Dictionary<string, string> headers)
+        {
+            return new FileTo(headers);
+        }
+    }
+}
---
+++ tests/Kyameru.Component.File.Tests/FileWatcherTests.cs
@@ -0,0 +1,90 @@
+﻿using Kyameru.Core.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace Kyameru.Component.File.Tests
+{
+    [TestFixture]
+    public class FileWatcherTests
+    {
+        private readonly string location;
+        private Routable message;
+
+        public FileWatcherTests()
+        {
+            this.location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void RenameWorks()
+        {
+            this.CheckFile("rename.tdd");
+            this.CheckFile("renamed.tdd");
+            AutoResetEvent resetEvent = new AutoResetEvent(false);
+            string method = string.Empty;
+            System.IO.File.WriteAllText($"{this.location}/rename.tdd", "test data");
+            FileWatcher from = this.Setup("Renamed");
+            from.OnAction += delegate (object sender, Routable e)
+            {
+                method = e.Headers["Method"];
+                resetEvent.Set();
+            };
+            from.Setup();
+            from.Start();
+            System.IO.File.Move($"{this.location}/rename.tdd", $"{this.location}/renamed.tdd");
+            bool wasAssigned = resetEvent.WaitOne(TimeSpan.FromSeconds(5));
+            Assert.AreEqual("Renamed", method);
+        }
+
+        [Test]
+        public void CreatedWorks()
+        {
+            this.CheckFile("created.tdd");
+            AutoResetEvent resetEvent = new AutoResetEvent(false);
+            string method = string.Empty;
+
+            FileWatcher from = this.Setup("Created");
+            from.OnAction += delegate (object sender, Routable e)
+            {
+                method = e.Headers["Method"];
+                resetEvent.Set();
+            };
+            from.Setup();
+            from.Start();
+            System.IO.File.WriteAllText($"{this.location}/Created.tdd", "test data");
+            bool wasAssigned = resetEvent.WaitOne(TimeSpan.FromSeconds(5));
+            Assert.AreEqual("Created", method);
+        }
+
+        private void From_OnAction(object sender, Core.Entities.Routable e)
+        {
+            this.message = e;
+        }
+
+        public File.FileWatcher Setup(string notification)
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Target", this.location },
+                { "Notifications", notification },
+                { "Filter", "*.*" },
+                { "SubDirectories", "true" }
+            };
+
+            return new FileWatcher(headers);
+        }
+
+        private void CheckFile(string file)
+        {
+            if (System.IO.File.Exists($"{location}/{file}"))
+            {
+                System.IO.File.Delete($"{location}/{file}");
+            }
+        }
+    }
+}
